12d3b5585c913be8e823ac4b71878771901f8d90
Beka Sturua <beqasturua3@gmail.com>
2022-06-27 22:57:31 +0400

    finished project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import UserFinder from "./components/UserFinder";
+import UserFinder from './components/UserFinder';
+import UsersContext from './store/users-context';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Beka' },
+  { id: 'u2', name: 'Anna' },
+  { id: 'u3', name: 'Nini' },
+  { id: 'u4', name: 'Ilia' },
+  { id: 'u5', name: 'David' },
+  { id: 'u6', name: 'George' },
+];
 
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS
+  }
+
   return (
-    <div>
+    <UsersContext.Provider value={usersContext}>
       <UserFinder />
-    </div>
+    </UsersContext.Provider>
   );
 }
 
